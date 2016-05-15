e08cc6098ecf3c53bf6d96339205ca7e41f44002
ransom <auvergnerw@gmail.com>
2016-05-15 14:42:29 -0700

    emacs: update modes by file ext

--- emacs/sys-config.el
+++ emacs/sys-config.el
@@ -26,5 +26,10 @@
   '(fringe ((t (:background "black")))))
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs$" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.rt$" . html-mode))
+
