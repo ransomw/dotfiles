92e244a12aa0de3117fd3eb614ac6de224a6bbe8
ransom williams <auvergnerw@gmail.com>
2025-12-16 15:31:36 -0600

    fish: init config.fish boilerplate

---
+++ config.fish
@@ -0,0 +1,8 @@
+# link to ~/.config/fish/config.fish
+
+# `fish_config` (the web-config, a builtin command) handles saving its settings automatically to ensure persistence.
+
+if status is-interactive
+
+end
+
